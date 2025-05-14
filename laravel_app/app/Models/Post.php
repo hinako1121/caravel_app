a0cfd81d5903abd5c2d53aa2dc2c07c25321f314
mitsuishi hinako <fukutaro166@gmail.com>
2025-05-15 02:40:02 +0900

    5/15　投稿データの一覧表示

--- laravel_app/app/Models/Post.php
+++ laravel_app/app/Models/Post.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    //use HasFactory;
 
     protected $fillable = [
         'title',
         'body',
+        'user_id'
     ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
