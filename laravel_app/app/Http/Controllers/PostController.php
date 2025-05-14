7f78d78ddf0fe067422d5f9a537c5b52121b3262
mitsuishi hinako <fukutaro166@gmail.com>
2025-05-15 01:49:41 +0900

    5/15 投稿データの作成と保存

---
+++ laravel_app/app/Http/Controllers/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function create() {
+        return view('post.create');
+    }
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'title' => 'required|max:20',
+            'body' => 'required|max:400',
+        ]);
+
+        $post = Post::create($validated)([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        $request->session()->flash('message','保存しました');
+
+        return back();
+    }
+}
